c7dd7c883f83d6f8394afd0b9424c1592dd64766
ariniastari <astariarini@gmail.com>
2014-09-15 21:09:25 +0700

    restructuring themes

---
+++ wp-content/themes/metaspace/dettol-intro.php
@@ -0,0 +1,98 @@
+<div class="row first" id="first">
+  <div class="col-md-6 col-xs-12">
+    <div class="cycle-slideshow" data-cycle-timeout="5000" data-cycle-speed="2000">
+      <?php
+        if( have_rows('intro_photo_gallery') ):
+
+            while ( have_rows('intro_photo_gallery') ) : the_row();
+      ?>
+
+                <img src="<?php the_sub_field('image_top_left'); ?>" alt="" width="100%" class="img-responsive">
+                
+      <?php
+            endwhile;
+
+        else :
+
+            // no rows found
+
+        endif;
+      ?>
+    </div>
+    <div class="cycle-slideshow" data-cycle-timeout="9000" data-cycle-speed="2000">
+      <?php
+        if( have_rows('intro_photo_gallery') ):
+
+            while ( have_rows('intro_photo_gallery') ) : the_row();
+      ?>
+
+                <img src="<?php the_sub_field('image_top_right'); ?>" alt="" width="100%" class="img-responsive">
+                
+      <?php
+            endwhile;
+
+        else :
+
+            // no rows found
+
+        endif;
+      ?>
+    </div>
+    <div class="cycle-slideshow" data-cycle-timeout="10000" data-cycle-speed="2000">
+      <?php
+        if( have_rows('intro_photo_gallery') ):
+
+            while ( have_rows('intro_photo_gallery') ) : the_row();
+      ?>
+
+                <img src="<?php the_sub_field('image_bottom_left'); ?>" alt="" width="100%" class="img-responsive">
+                
+      <?php
+            endwhile;
+
+        else :
+
+            // no rows found
+
+        endif;
+      ?>
+    </div>
+    <div class="cycle-slideshow" data-cycle-timeout="7000" data-cycle-speed="2000">
+      <?php
+        if( have_rows('intro_photo_gallery') ):
+
+            while ( have_rows('intro_photo_gallery') ) : the_row();
+      ?>
+
+                <img src="<?php the_sub_field('image_bottom_right'); ?>" alt="" width="100%" class="img-responsive">
+                
+      <?php
+            endwhile;
+
+        else :
+
+            // no rows found
+
+        endif;
+      ?>
+    </div>
+    <div class="btn-video" data-toggle="modal" data-target="#modal-video">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/video.png" alt="">
+      <span>LIHAT VIDEO</span>
+    </div>
+  </div>
+  <div class="col-md-6 col-xs-12">
+    <?php the_field('intro_description'); ?>
+    <a href="http://localhost:8888/wp-login.php?loginFacebook=1"><img src="<?php echo get_template_directory_uri(); ?>/assets/img/btn-dukung.png" alt="" class="right btn-dukung"></a>
+  </div>
+  <div class="modal fade" id="modal-video">
+    <div class="modal-dialog">
+      <div class="modal-content">
+        <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+        <div class="modal-body clearfix">
+          <embed width="100%" height="100%" src="http://www.youtube.com/v/jqrE98W-gBI">
+        </div>
+      </div><!-- /.modal-content -->
+    </div><!-- /.modal-dialog -->
+  </div><!-- /.modal -->
+</div>
---
+++ wp-content/themes/metaspace/dettol-need.php
@@ -0,0 +1,75 @@
+<div class="row second" id="second">
+  <div class="col-md-4 col-xs-12">
+    <div class="left-cont">
+      <!-- <img src="<?php echo get_template_directory_uri(); ?>/assets/img/bg-second.jpg" class="img-responsive" alt=""> -->
+      <h3>mereka yang membutuhkan</h3>
+      <div class="separator"></div>
+      <?php the_field("second_description"); ?>
+    </div>
+  </div>
+  <div class="col-md-8 carousel col-xs-12">
+    <div class="jcarousel">
+      <ul>
+        <?php
+          $values = get_field('second_gallery');
+          if($values) {
+            // dd($values);
+            $counter = 1;
+            foreach ($values as $value) {
+              if ($counter % 2 != 0){ ?>
+              <li>
+                <img src="<?php echo $value['second_gallery_image']; ?>" alt="" class="img-responsive" data-toggle="modal" data-target="#<?php echo $value['gallery_id'] ?>">
+              <?php 
+              }
+              else { ?>
+                <img src="<?php echo $value['second_gallery_image']; ?>" alt="" class="img-responsive" data-toggle="modal" data-target="#<?php echo $value['gallery_id'] ?>">
+              </li>
+              <?php
+              $counter = 1; 
+              }
+              $counter ++;
+            }
+
+            if (count($values) % 2 != 0){ ?>
+              </li>
+            <?php
+            }
+          }
+        ?>
+      </ul>
+    </div>
+    <div class="jcarousel-pagination">
+        <!-- Pagination items will be generated in here -->
+    </div>
+
+    <?php
+      if( have_rows('second_gallery') ):
+
+          while ( have_rows('second_gallery') ) : the_row();
+    ?>
+      <div class="modal fade" id="<?php the_sub_field("gallery_id"); ?>">
+      <div class="modal-dialog">
+        <div class="modal-content modal-butuh">
+          <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+          <div class="modal-body clearfix">
+            <div class="col-md-6 col-xs-12">
+              <img src="<?php the_sub_field("image_inside"); ?>" alt="" class="img-responsive">
+            </div>
+            <div class="col-md-6 col-xs-12">
+              <?php the_sub_field("second_gallery_text"); ?>
+            </div>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+    <?php
+          endwhile;
+
+      else :
+
+          // no rows found
+
+      endif;
+    ?>
+  </div>
+</div>
