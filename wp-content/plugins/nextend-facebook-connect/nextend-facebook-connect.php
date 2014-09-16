40b29a8290695a00e5a7e952651a70011d29b615
ariniastari <astariarini@gmail.com>
2014-09-16 18:23:29 +0700

    disable fb login in wp-login

--- wp-content/plugins/nextend-facebook-connect/nextend-facebook-connect.php
+++ wp-content/plugins/nextend-facebook-connect/nextend-facebook-connect.php
@@ -364,9 +364,9 @@ function new_add_fb_login_form() {
       if(!has_social_form){
         has_social_form = true;
         var loginForm = $('#loginform,#registerform,#front-login-form');
-        socialLogins = $('<div class="newsociallogins" style="text-align: center;"><div style="clear:both;"></div></div>');
+        socialLogins = $('<div class="newsociallogins" style="display:none; text-align: center;"><div style="clear:both;"></div></div>');
         if(loginForm.find('input').length > 0)
-          loginForm.prepend("<h3 style='text-align:center;'>OR</h3>");
+          loginForm.prepend("<h3 style='display: none; text-align:center;'>OR</h3>");
         loginForm.prepend(socialLogins);
         socialLogins = loginForm.find('.newsociallogins');
       }
