3be904cb875c3a7836829237eef2607eddb54836
ariniastari <astariarini@gmail.com>
2014-09-16 12:50:56 +0700

    update menu

---
+++ wp-content/themes/metaspace/dettol-menu.php
@@ -0,0 +1,41 @@
+<div class="navbar navbar-default navbar-fixed-top" role="navigation">
+  <div class="navbar-header">
+    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+      <span class="icon-bar"></span>
+    </button>
+    
+    <div class="logo-bg">
+      <a class="navbar-brand" href="#first">
+        <img src="assets/img/logo-inner.png" alt="">
+      </a>
+      <!-- ADD HIDDEN MD -->
+      <a href="http://www.dettol.co.id" target="_blank" class="hidden-lg hidden-sm hidden-md">
+        <img src="assets/img/logo-right.png" alt="" class="img-responsive">
+      </a>
+      <!-- END OF ADD HIDDEN MD -->
+    </div>
+  </div>
+  <div class="navbar-collapse collapse">
+    <ul class="nav navbar-nav">
+      <li><a href="#second">mereka yang membutuhkan</a></li>
+      <li><a href="#third">Dukungan yang diterima</a></li>
+      <li><a href="#third">wall of pictures</a></li>
+      <li class="active"><a href="" data-toggle="modal" data-target="#modal-thanks">beri <br/>dukungan</a></li>
+    </ul>
+  </div><!--/.nav-collapse -->
+  <!-- ADD HIDDEN SM -->
+  <div class="navbar-right hidden-xs">
+    <a href="http://www.dettol.co.id" target="_blank">
+      <img src="assets/img/logo-right.png" alt="" class="img-responsive">
+    </a>
+    <a href="https://www.facebook.com/DettolIndonesia" class="hidden-sm">
+      <img src="assets/img/fb-header.png" alt="" class="img-responsive">
+    </a>
+    <a href="https://twitter.com/Dettol_ID" class="hidden-sm">
+      <img src="assets/img/twitter-header.png" alt="" class="img-responsive">
+    </a>
+  </div>
+  <!-- END OF ADD HIDDEN SM -->
+</div>
--- wp-content/themes/metaspace/index.php
+++ wp-content/themes/metaspace/index.php
@@ -14,43 +14,7 @@
     <div id="wrap">
 
       <!-- Fixed navbar -->
-      <div class="navbar navbar-default navbar-fixed-top" role="navigation">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target=".navbar-collapse">
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          
-          <div class="logo-bg">
-            <a class="navbar-brand" href="#first">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-inner.png" alt="">
-            </a>
-            <a href="http://www.dettol.co.id" target="_blank" class="hidden-lg hidden-sm">
-              <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-right.png" alt="" class="img-responsive">
-            </a>
-          </div>
-        </div>
-        <div class="navbar-collapse collapse">
-          <ul class="nav navbar-nav">
-            <li><a href="#second">mereka yang membutuhkan</a></li>
-            <li><a href="#third">Dukungan yang diterima</a></li>
-            <li><a href="#third">wall of pictures</a></li>
-            <li class="active"><a href="" data-toggle="modal" data-target="#modal-thanks">beri <br/>dukungan</a></li>
-          </ul>
-        </div><!--/.nav-collapse -->
-        <div class="navbar-right hidden-xs hidden-sm hidden-md">
-          <a href="http://www.dettol.co.id" target="_blank">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-right.png" alt="" class="img-responsive">
-          </a>
-          <a href="https://www.facebook.com/DettolIndonesia">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fb-header.png" alt="" class="img-responsive">
-          </a>
-          <a href="https://twitter.com/Dettol_ID">
-            <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter-header.png" alt="" class="img-responsive">
-          </a>
-        </div>
-      </div>
+      <?php get_template_part('dettol', 'menu'); ?>
 
       <!-- Begin page content -->
       <?php get_template_part('dettol', 'intro'); ?>
