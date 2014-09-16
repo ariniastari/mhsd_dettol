4335cb8a337b40a183d0926f24422a77c67c2b96
ariniastari <astariarini@gmail.com>
2014-09-16 21:19:55 +0700

    add meta tags for facebook

--- wp-content/themes/metaspace/header.php
+++ wp-content/themes/metaspace/header.php
@@ -6,6 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta property="og:title" content="Misi Hidup Sehat Dettol" />
+    <meta property="og:site" content="www.misihidupsehatdettol.com
+" />
+    <meta property="og:description" content="Di Indonesia, risiko penyebab kematian utama pada anak adalah diare dan infeksi saluran pernapasan. Risiko ini dapat diturunkan lebih dari setengahnya dengan cuci tangan menggunakan sabun. 
+Saya mendukung inisiatif Misi Hidup Sehat Dettol membangun ratusan fasilitas cuci tangan di tempat-tempat yang membutuhkan.
+1 Klik untuk 1 anak yang membutuhkan. Ayo ikut dukung Misi Hidup Sehat Dettol agar lebih banyak anak terlindungi!
+" />
+    <meta property="og:image" content="<?php echo get_template_directory_uri(); ?>/assets/img/logo-inner.png" />
 
     <!-- CSS -->
     <link href="<?php echo get_template_directory_uri(); ?>/assets/css/bootstrap.css" rel="stylesheet">
