37484bd348e6a50ce6ddaaf5105fcb77215432c7
alditayurio <aldita.yurioputri@veritrans.co.id>
2014-10-08 00:08:43 +0700

    fixin img header

--- wp-content/themes/metaspace/dettol-menu.php
+++ wp-content/themes/metaspace/dettol-menu.php
@@ -25,13 +25,13 @@
   <!-- ADD HIDDEN SM -->
   <div class="navbar-right hidden-xs hidden-sm">
     <a href="http://www.dettol.co.id" target="_blank">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-right.png" alt="" class="img-responsive">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/logo-right.png" alt="" class="img-responsive img-logo-right">
     </a>
     <a href="https://www.facebook.com/DettolIndonesia" class="hidden-sm hidden-md" target="_blank">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fb-header.png" alt="" class="img-responsive">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/fb-header.png" alt="" class="img-responsive img-socmed-right">
     </a>
     <a href="https://twitter.com/Dettol_ID" class="hidden-sm hidden-md" target="_blank">
-      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter-header.png" alt="" class="img-responsive">
+      <img src="<?php echo get_template_directory_uri(); ?>/assets/img/twitter-header.png" alt="" class="img-responsive img-socmed-right">
     </a>
   </div>
   <!-- END OF ADD HIDDEN SM -->
